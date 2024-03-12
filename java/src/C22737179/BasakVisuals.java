cfd3005b02f4972820e3ced0038395cf2f2a9c0b
Basak Deveci <basakdeveci99@hotmail.com>
2024-03-12 02:20:23 +0000

    projectVisual

--- java/src/C22737179/BasakVisuals.java
+++ java/src/C22737179/BasakVisuals.java
@@ -7,7 +7,7 @@ public class BasakVisuals extends Visual {
     float x=300;
     float y=300;
     float angle;
-    float dia=10;
+    
 
     //float smoothedEllipseSize = 0;
     
@@ -15,7 +15,6 @@ public class BasakVisuals extends Visual {
         //calculateAverageAmplitude();
 
         this.g = e.getGraphics(); // Initialize the "g" variable
-        //background(0);
         //stroke(255);
         //line(50, 450, 400, 750);
         //calculateAverageAmplitude();
@@ -27,9 +26,10 @@ public class BasakVisuals extends Visual {
         for(float a=0; a<360; a+=10){
             pushMatrix();
             rotate(radians(a));
+            lights();
 
             float col = map(angle, 0, 700, 0, 255);
-            //float col2 = map(a,35,255,0,450);
+           // float col2 = map(a,0,500,0,250);
             
 
             //float ellipseSize = 50 + (getAmplitude() * 300);
@@ -37,22 +37,23 @@ public class BasakVisuals extends Visual {
     
             
             stroke(col,120, 255);
-           // stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+            // stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
             //stroke(255);
-            strokeWeight(3);
+            strokeWeight(1);
+            
             
             line(x*sin(radians(angle)),0,0,y);
 
-            ellipse(x*sin(radians(angle)),0,dia,dia);
+            ellipse(x*sin(radians(angle)),0,10,10);
 
-            //box(ellipseSize);
+            triangle(x * sin(radians(angle)), 0, 0, 5, 0, 0);
+
+            ellipse(0,y,10,10);
             
-
-            ellipse(0,y,dia,dia);
             popMatrix();
         }
 
-        angle+=2;
+        angle+=1;
     }
         
         
