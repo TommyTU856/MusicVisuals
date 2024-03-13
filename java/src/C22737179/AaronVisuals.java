c48b78ed2c55f8ef50c06585085c869d65afbe58
AaronTU856 <124066652+AaronTU856@users.noreply.github.com>
2024-03-13 19:26:38 +0000

    Aaron Update

--- java/src/C22737179/AaronVisuals.java
+++ java/src/C22737179/AaronVisuals.java
@@ -2,16 +2,38 @@ package C22737179;
 
 import ie.tudublin.*;
 
-public class AaronVisuals extends Visual {
+public class AaronVisuals extends Visual 
+{
     
-    public void render(CallSet e) {
+    float angle = 0.0f;
+    float offset = 60;
+    float scalar = 2;
+    float speed = 0.05f;
+
+
+    public void draw(CallSet e) 
+    {
         
         this.g = e.getGraphics(); // Initialize the "g" variable
         background(0);
         stroke(255);
-        line(50, 450, 400, 750);
+        smooth();
+        fill(0);
+        rotate(angle);
+        float x = offset + cos(angle) * scalar;
+        float y = offset + sin(angle) * scalar;
+        ellipse( x, y, 100, 100);
+        angle += speed;
+        scalar += speed;
+
+       
+
+
+
+        
+        }
+
 
     }
 
 
-}
