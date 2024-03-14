fb58436091ce39a22e37a2028612c783169c9a4d
TommyTU856 <124066720+TommyTU856@users.noreply.github.com>
2024-03-14 23:02:52 +0000

    made it so the drawing pauses when the music does

--- java/src/C22737179/TommyVisuals.java
+++ java/src/C22737179/TommyVisuals.java
@@ -1,5 +1,6 @@
 package C22737179;
 
+import ddf.minim.AudioPlayer;
 import ie.tudublin.*;
 
 public class TommyVisuals extends Visual{
@@ -19,19 +20,22 @@ public class TommyVisuals extends Visual{
     }
     
     public void draw(CallSet e) {
-        
-        this.g = e.getGraphics();
-        colorMode(HSB, 360, 100, 100);
-        background(80, 100, 100);
 
-        translate(w(0.5f), h(0.5f));
-        for(int i = 0; i < numLollipops; i++) {
-            pushMatrix();
-            lollipops[i].display(e);
-            lollipops[i].update(); 
-            popMatrix();       
-        }
+        this.g = e.getGraphics();
         
+        if(e.paused == false) {
+            
+            colorMode(HSB, 360, 100, 100);
+            background(80, 100, 100);
+
+            translate(w(0.5f), h(0.5f));
+            for(int i = 0; i < numLollipops; i++) {
+                pushMatrix();
+                lollipops[i].display(e);
+                lollipops[i].update(); 
+                popMatrix();       
+            }
+        }
         
     }
 
