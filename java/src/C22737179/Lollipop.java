bd006d38d205de2af84ebcd50d0686939a1ed80f
TommyTU856 <124066720+TommyTU856@users.noreply.github.com>
2024-03-14 20:31:45 +0000

    Lost all fuction due to 2 class files of the same name open and updated the wrong one, issue resolved TB

--- java/src/C22737179/Lollipop.java
+++ java/src/C22737179/Lollipop.java
@@ -1,45 +1,53 @@
 package C22737179;
 
-public class Lollipop extends TommyVisuals{
+import ie.tudublin.*;
+
+public class Lollipop extends Visual {
 
 
-    /* Lollipop[] lollipops;
-    int numLollipops = 25; */
     float angle;
     float angleSpeed;
     float radius;
 
-    TommyVisuals t;
+    public float getRadius() {
+        return radius;
+    }
 
-    Lollipop(int n) {
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+
+    public Lollipop(int n, float radius){
         this.angle = 0;
         this.angleSpeed = 0.01f + n * 0.001f;
         this.radius = 30 + n * 6;
+        
     }
 
     void update() {
         angle += angleSpeed;
     }
 
-    void display() {
+    void display(CallSet e) {
         
-        lights();
-        calculateAverageAmplitude();
-        stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        e.lights();
+        e.calculateAverageAmplitude();
+        e.stroke(map(e.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
 
         float x = radius * cos(angle);
         float y = radius * sin(angle);
 
         //println(getSmoothedAmplitude());
         //line(0, 0, x, y);
-        noStroke();
-        if (getSmoothedAmplitude() > 0.1) {
-            fill(random(0,360), 80, 100);
+        e.noStroke();
+        if (e.getSmoothedAmplitude() > 0.1) {
+            e.fill(random(0,360), 80, 100);
         }
         
-        translate(x, y);
-        float sphereSize = 25 + (200 * getSmoothedAmplitude()); 
-        sphere(sphereSize);
+        e.translate(x, y);
+        float sphereSize = 25 + (200 * e.getSmoothedAmplitude()); 
+        e.sphere(sphereSize);
         //ellipse(x, y, 25 + getSmoothedAmplitude(), 25 + getSmoothedAmplitude());
     }
 
