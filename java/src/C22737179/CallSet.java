2163ed8d8519b672bc74534062faba5df5fe5d47
TommyTU856 <124066720+TommyTU856@users.noreply.github.com>
2024-03-17 16:00:49 +0000

    got the rooster displaying but having issues with class seperation and getting objects to call properly

--- java/src/C22737179/CallSet.java
+++ java/src/C22737179/CallSet.java
@@ -83,7 +83,7 @@ public class CallSet extends Visual {
     AaronVisuals Aaron = new AaronVisuals();
     AniketVisuals Aniket = new AniketVisuals();
     BasakVisuals Basak = new BasakVisuals();
-    TommyVisuals Tommy = new TommyVisuals();
+    TommyVisuals Tommy = new TommyVisuals(this.g);
     
 
     public void draw()
--- java/src/C22737179/Intro.java
+++ java/src/C22737179/Intro.java
@@ -1,29 +1,57 @@
 package C22737179;
 
 import ie.tudublin.*;
+import processing.core.PGraphics;
 import processing.core.PShape;
 
 public class Intro extends Visual{
     
+        
+        float startTime;
+        float speed;
+        float roosterX;
         PShape rooster;
     
-        public Intro() {
+        public Intro(PGraphics pg) {
+            this.g = pg;
+            this.rooster = g.loadShape("GS0D6S94RVZNHC4AZDRXMJDLF.obj");
+            this.speed = g.width / 5.0f;
+            this.roosterX = -100;
+            this.startTime = millis();
+        }
+
+        public void draw(CallSet e) {
+
+            this.g = e.getGraphics();
+            e.background(255);
+            rooster();
+
         }
 
         public void rooster() {
 
             pushMatrix();
-            rooster = loadShape("GS0D6S94RVZNHC4AZDRXMJDLF.obj");
+            
+            float elapsedTime = millis() - startTime;
+
+            roosterX = (elapsedTime / 1000.0f) * speed;
+
+            if(roosterX < g.width / 2) {
+                translate(roosterX, g.height / 2);
+            } else {
+                translate(g.width / 2, g.height / 2);
+            }
+
+            
+            //translate(g.width / 2, g.height / 2);
+            rotateX(0);
+            rotateY(45);
+            rotateZ(210);
             scale(200);
             shape(rooster);
             popMatrix();
 
         }
     
-        public void draw(CallSet e) {
-            this.g = e.getGraphics();
-            e.background(255);
-            rooster();
-
-        }
+        
 }
