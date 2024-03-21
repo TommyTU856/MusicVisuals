c262f7d17549df828451648d6ba03939c6385e7a
TommyTU856 <124066720+TommyTU856@users.noreply.github.com>
2024-03-21 21:53:34 +0000

    fixed sketch location on screen issue

--- java/src/C22737179/ResponsiveShape.java
+++ java/src/C22737179/ResponsiveShape.java
@@ -4,25 +4,5 @@ import ie.tudublin.Visual;
 
 public class ResponsiveShape {
 
-    public ResponsiveShape() {
-        
-    }
 
-    public void render(CallSet e) {
-
-        e.background(0);
-
-        e.translate(e.width/2, e.height/2, 0);
-        e.stroke(255);
-        e.rotateX(Visual.PI/2);
-        e.rotateZ(-Visual.PI/6);
-        e.noFill();
-
-        e.beginShape();
-        
-        e.vertex(-100, -100, -100);
-        e.vertex( 100, -100, -100);
-        e.vertex(   0,    0,  100);
-        e.endShape();
-    }
 }
