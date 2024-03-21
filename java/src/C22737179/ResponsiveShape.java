467df2551fe0fd9a0e9e7584659ecc34eadffd7c
TommyTU856 <124066720+TommyTU856@users.noreply.github.com>
2024-03-21 21:55:28 +0000

    Starting ResponsiveShape- my main visual

--- java/src/C22737179/ResponsiveShape.java
+++ java/src/C22737179/ResponsiveShape.java
@@ -4,5 +4,22 @@ import ie.tudublin.Visual;
 
 public class ResponsiveShape {
 
+    int vert = 0;
 
+    public ResponsiveShape() {
+        
+    }
+
+    public void render(CallSet e) {
+
+        e.background(0);
+
+        for(int i = 0; i < vert; i++) {
+            for(int j = 0; j < vert; j++) {
+                for(int k = 0; k < vert; k++) {
+                    
+                }
+            }
+        }
+    }
 }
