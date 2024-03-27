ed4ee0830d25382895bc6090c417cf7f941e16ae
AaronTU856 <124066652+AaronTU856@users.noreply.github.com>
2024-03-27 17:40:05 +0000

    Changes to clear screen and transition of objects

--- java/src/C22737179/AaronVisuals.java
+++ java/src/C22737179/AaronVisuals.java
@@ -1,10 +1,9 @@
 package C22737179;
 
 import ie.tudublin.*;
+import processing.core.PShape;
 
-
-public class AaronVisuals extends Visual 
-{
+public class AaronVisuals extends Visual {
     DrawGrid grid;
     TimeClock timeClock;
     Fade fade;
@@ -16,29 +15,38 @@ public class AaronVisuals extends Visual
 
     int numEllipses = 70; // Number of ellipses
     float[][] ellipses = new float[numEllipses][4]; // Array to store ellipse coordinates and color
-    
-    public void draw(CallSet e) 
-    {
+
+    boolean clearScreen = true;
+    int shapeColor;
+
+    public AaronVisuals() {
+    }
+
+    public void draw(CallSet e) {
         this.g = e.getGraphics(); // Initialize the "g" variable
-        grid = new DrawGrid(this);
+        //grid = new DrawGrid(this);
         timeClock = new TimeClock(this);
         fade = new Fade(100, 100, 1000, this); // Initialize Fade object with specific values
-        
+
         colorMode(RGB);
-        background(color(130, 230, 330)); // Use color() function for background color
-        
+        if (clearScreen) {
+            background(0); // Use color() function for background color (black)
+            clearScreen = false;
+            shapeColor = color(random(255), random(255), random(255));
+        }
+
         fade.update();
         fade.display();
         timeClock.drawTimeClock();
-        
+
         stroke(255);
-        fill(255, 255, 0);
-        translate(g.width / 2, g.height / 2); // Center the shapes
+        fill(200); // Set fill color to light gray
+        translate(width / 2, height / 2); // Center the shapes
         smooth();
         rotate(angle);
 
-        float centerX = g.width / 2;
-        float centerY = g.height / 2;
+        float centerX = width / 2;
+        float centerY = height / 2;
         float x = centerX;
         float y = centerY;
 
@@ -46,16 +54,11 @@ public class AaronVisuals extends Visual
         ellipse(x, y, 50, 50);
 
         // Generating and drawing small random ellipses
-        for (int i = 0; i < numEllipses; i++) 
-        {
+        for (int i = 0; i < numEllipses; i++) {
             float x2 = random(g.width); // Generate random x-coordinate
             float y2 = random(g.height); // Generate random y-coordinate
-            
-            int r = (int)(random(255)); // Generate random red component
-            int g = (int)(random(255)); // Generate random green component
-            int b = (int)(random(255)); // Generate random blue component
-            
-            fill(r, g, b);
+
+            fill(200); // Set fill color to light gray
             ellipse(x2, y2, 10, 10); // Draw ellipse at random location
         }
 
@@ -70,34 +73,35 @@ public class AaronVisuals extends Visual
         float circleX = x + 10;
         float circleY = y + 10;
 
-        fill(105, 0, 105);
+        fill(200); // Set fill color to light gray
         ellipse(150, -150, 200, 200);
-        fill(105, 205, 170);
+        fill(200); // Set fill color to light gray
         circle(300, -300, x1);
+        fill(200); // Set fill color to light gray
         ellipse(circleX, -circleY, 250, 250);
 
         float circleX1 = x1;
         float circleY1 = y1;
 
-        fill(205, 205, 0);
-        circle(circleX1, -circleY1, 220); 
+        fill(200); // Set fill color to light gray
+        circle(circleX1, -circleY1, 220);
         angle += speed;
         scalar += speed;
-       
-        translate(-g.width / 3, -g.height / 3); // Move origin to a different location
-        fill(20, 255, 20); 
+
+        translate(-width / 3, -height / 3); // Move origin to a different location
+        fill(200); // Set fill color to light gray
         rect(400, -400, 150, 150); // Draw a rectangle at the new origin
-       
-        angle += speed; 
-        drawGrid(g.width / 3, g.height / 3, g.width, g.height); // Draw grid   
+
+        angle += speed;
+        //drawGrid(g.width / 3, height / 3, width, height); // Draw grid
     }
-    
-    void drawGrid(int x, int y, int width, int height) {
-        for (int gridY = 0; gridY < height; gridY += height / 10){
-            for (int gridX = 0; gridX < width; gridX += width / 10){
-                this.line(gridX, gridY, gridX + width / 10, gridY + height / 10);
-                this.line(gridX + width / 10, gridY, gridX, gridY + height / 10);
-            }
-        }
-    }
+
+    // void drawGrid(int x, int y, int width, int height) {
+    //     for (int gridY = 0; gridY < height; gridY += height / 10) {
+    //         for (int gridX = 0; gridX < width; gridX += width / 10) {
+    //             this.line(gridX, gridY, gridX + width / 10, gridY + height / 10);
+    //             this.line(gridX + width / 10, gridY, gridX, gridY + height / 10);
+    //         }
+    //     }
+    // }
 }
