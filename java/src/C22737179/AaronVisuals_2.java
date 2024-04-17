7899fdeff9afae124a008fc85325f1cf70b0210e
AaronTU856 <124066652+AaronTU856@users.noreply.github.com>
2024-04-17 21:18:56 +0100

    Added a colourful cube and made lots of changes

--- java/src/C22737179/AaronVisuals_2.java
+++ java/src/C22737179/AaronVisuals_2.java
@@ -7,27 +7,43 @@ import ddf.minim.*;
 
 public class AaronVisuals_2 extends Visual {
 
-   
-    
-    
-    
-    public AaronVisuals_2()
-    {
+    int sphereRadius;
+    float spherePrevX;
+    float spherePrevY;
+    int yOffset;
+    boolean initialStatic = true;
+    float[] extendingSphereLinesRadius;
+    int OFF_MAX = 300;
+
+    public AaronVisuals_2() {
         
     }
 
     public void draw(CallSet e) {
-
-        e.background(255, 0xFF, 0); 
-		e.stroke(0, 255, 0);
-		e.line(10, 10, 100, 100); // x1, y1, x2, y2
-		e.fill(90, 0, 0);
-		e.ellipse(200, 90, 80, 200); // cx, cy
-		e.circle(200, 300, 80);
-		e.fill(90, 78, 88);
-		e.rect(400, 380, 50, 100); // x, y, w, h
-		e.triangle(10, 200, 200, 200, 300, 400);
-
-       
+        
+        e.background(0);
+        e.translate(e.width / 2, e.height / 2, -OFF_MAX);
+        e.rotateX(e.frameCount * .01f);
+        e.rotateY(e.frameCount * .01f);
+        e.rotateZ(e.frameCount * .01f);
+        
+        for (int xo = -OFF_MAX; xo <= OFF_MAX; xo += 50) {
+            for (int yo = -OFF_MAX; yo <= OFF_MAX; yo += 50) {
+                for (int zo = -OFF_MAX; zo <= OFF_MAX; zo += 50) {
+                    e.pushMatrix();
+                    e.translate(xo, yo, zo);
+                    e.rotateX(e.frameCount * .02f);
+                    e.rotateY(e.frameCount * .02f);
+                    e.rotateZ(e.frameCount * .02f);
+                    e.fill(colorFromOffset(xo), colorFromOffset(yo), colorFromOffset(zo));
+                    e.box((float) (20 + (Math.sin(e.frameCount / 20.0)) * 15));
+                    e.popMatrix();
+                }
+          }
+        }
+      }
+      
+      int colorFromOffset(int offset) {
+        return (int) ((offset + OFF_MAX) / (2.0 * OFF_MAX) * 255);
+      }
     }
-}
