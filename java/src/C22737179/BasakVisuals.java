fe6c5f781fe13d3ea84859b763a3e9ac91652002
Basak Deveci <basakdeveci99@hotmail.com>
2024-04-22 19:28:26 +0100

    clouds

--- java/src/C22737179/BasakVisuals.java
+++ java/src/C22737179/BasakVisuals.java
@@ -1,52 +1,29 @@
 package C22737179;
 
 import ie.tudublin.*;
-import jogamp.opengl.glu.mipmap.Image;
 import processing.core.PImage;
 
 public class BasakVisuals extends Visual {
 
     
-    float x=100*2+100;
-    float y=100*2+50;
+    float x=50*2+100;
+    float y=80*2+50;
     float angle;
     int value=300;
     Ellipse ellipse;
     cube2 cube2;
 
-    float height=1.5f;
-    float width=1.5f;
-    float length=1.5f;
-  
-    RandomizedCubePattern randomizedCubePattern;
-
-
-    PImage backgroundImage; // Declare a variable to hold the background image
-
-   
-
-    // Setup method to load the background image
     
+    PImage backgroundImage;
+
     
 
     public BasakVisuals() {
         this.ellipse=new Ellipse(x ,y, angle, value);
         this.cube2=new cube2();
-        this.randomizedCubePattern=new RandomizedCubePattern();
-      
-        
        
-      
-    
     }
-
-    public void setup() {
-        
     
-       
-       
-        
-    }
 
     
     
@@ -54,22 +31,14 @@ public class BasakVisuals extends Visual {
        
         this.g = e.getGraphics();
         background(0);
-
-        e.image(e.cloudImage, 0, 0, width, height);
-        e.translate(e.width/2,e.height/2);
-        
-        //e.backgroundImage.resize(g.width, g.height);
-        //e.background(e.backgroundImage);
-
-        
-        //translate(g.width/2,g.height/2);
-
+       
+        //e.translate(e.width/2,e.height/2);
         colorMode(HSB);
         
 
-        ellipse.draw(e);
-        //cube2.draw(e);
-        //randomizedCubePattern.draw(e);
+        //ellipse.draw(e);
+        cube2.draw(e);
+       
      
 
         
---
+++ java/src/C22737179/Clouds.java
@@ -0,0 +1,49 @@
+package C22737179;
+import ie.tudublin.*;
+
+public class Clouds extends Visual {
+
+    float x,y,z,speed;
+
+    public Clouds(float cloudX, float cloudY,float z,float speed){
+        this.x=cloudX;
+        this.y=cloudY;
+        this.z=z;
+        this.speed=speed;
+
+
+    }
+
+    public void setSize(float newSize) {
+        z = newSize;
+       
+    }
+
+
+    void display(CallSet e)
+    {
+        //e.imageMode(CENTER);
+        e.image(e.cloudImage,x,y,z,z);
+    }
+
+    void move(CallSet e,Clouds[] allClouds){
+        x += speed; // Move the cloud towards the left
+
+    // Wrap around to the left side of the screen if the cloud moves beyond the right boundary
+        if (x > e.width) {
+            x = random(0, e.width); // Reset x position to a random position within the screen width
+            y = random(e.height); // Reset y position to a random height
+        }
+
+        // Check for overlap with other clouds and adjust position if necessary
+        for (Clouds otherCloud : allClouds) {
+            if (otherCloud != this && dist(x, y, otherCloud.x, otherCloud.y) < (z + otherCloud.z) / 2) {
+                // If the cloud overlaps with another cloud, adjust its position to the right of the overlapping cloud
+                x = otherCloud.x + otherCloud.z + 120;
+               // Adjust the spacing between clouds
+               
+            }
+        }
+    }
+}
+
